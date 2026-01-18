0603731b9ad057ad919853ab9aaa67f32df9b5ca
Advait Rathish <advaitrathish@gmail.com>
2026-01-18 13:01:03 +0530

    Day 3- API mindset, App routing, API view

--- queues/views.py
+++ queues/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def api_test(request):
+    return HttpResponse("API working fine!")
--- waitwise/urls.py
+++ waitwise/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from queues.views import api_test
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+path("api-test/", api_test),
